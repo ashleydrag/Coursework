3487dbb10858e3564ebdcf59b2708884c1a0d53b
ashleydrag <ashley.drag@simonpearce.com>
2020-03-10 16:01:25 -0400

    Updated the notes

--- ExampleNotes.R
+++ ExampleNotes.R
@@ -1,3 +1,12 @@
 Notes:
   Reference article be found here:
   Microsoft Dynamics>Tools>Development Tools> Cross Reference
+
+Fraud software notes:
+  1. order management parameters -? fraud check box is checked that means it's on. when it is off then it is off for te entire company'
+  2. fraud - static data - a place to add parameters, right now this is being used for phone #'s
+  3. fraud - variable - fraud conditions - not being used right now, can be used to define dynamic variables by table set up monitoring conditionMessage
+  4. fraud - conditions - conditions we are uing now to monitor fraud
+  5. Fraud - rules - defines fruad rules based on static data,variables, and conditions
+  6. To see holds - under reports holds
+  7. Sales order details - this is under secondary status
